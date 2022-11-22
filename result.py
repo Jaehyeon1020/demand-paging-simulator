a41961036726161c9bb563b473a0dbb62f13d882
김재현 <kjhkk1020@naver.com>
2022-11-22 17:24:58 +0900

    fix(update_result()): 리스트 깊은 복사하도록 수정

--- result.py
+++ result.py
@@ -7,8 +7,10 @@ class Result:
         self.total_page_faults = 0
         self.memory_info = [] # 각 time마다의 reference string, memory state, page fault 여부를 저장하는 리스트를 저장
     
+    # 각 time마다 호출할 함수
     def update_result(self, ref_string: str, memory_state: list, is_page_fault: bool):
-        self.memory_info.append([ref_string, memory_state, is_page_fault])
+        self.memory_info.append([ref_string, memory_state.copy(), is_page_fault])
 
-    def incread_total_page_fault(self):
+    # page fault가 발생할 때마다 호출할 함수
+    def increase_total_page_fault(self):
         self.total_page_faults += 1
