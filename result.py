55968cdea0ea28b0c20f2fec6832458055a1a382
김재현 <kjhkk1020@naver.com>
2022-11-22 22:04:11 +0900

    fix(print_result): 출력 형태 수정

--- result.py
+++ result.py
@@ -7,7 +7,7 @@ class Result:
         self.type = type
         self.total_page_faults = 0
         self.memory_info = [] # 각 time마다의 reference string, memory state, page fault 여부를 저장하는 리스트를 저장
-        self.type = None # 어떤 type의 replacing 기법을 사용하였는지 저장
+        self.type = type # 어떤 type의 replacing 기법을 사용하였는지 저장
     
     # 각 time마다 호출할 함수
     def update_result(self, ref_string: str, memory_state: list, is_page_fault: bool):
@@ -18,24 +18,25 @@ class Result:
         self.total_page_faults += 1
 
     def print_result(self):
-        time = 0
+        time = 1
 
-        print("=============== %s ALGORITHM RESULT ==============="%self.type)
+        print("\n=============== %s ALGORITHM RESULT ==============="%self.type)
         for result in self.memory_info:
-            print("<AT TIME %d>"%(time + 1))
-            print("Reference String: %s"%self.memory_info[time][0])
-            print("Memory State: ", end='')
-            print(self.memory_info[time][1])
-            print("Page Fault: ", end='')
+            print("<AT TIME %d>"%time)
+            print("Reference string: %s"%result[0]) # print ref string
+            print("Memory state: ", end='')
+            print(result[1]) # print memory state
+            print("Page fault at time %d : "%time, end='')
 
-            if self.memory_info[time][2]:
+            # print whether page fault is at this time
+            if result[2]:
                 print("YES")
             else:
                 print("NO")
 
-            print("--------------------")
+            print("----------------------------------------------------")
 
             time += 1
         
-        print("Total Page Fault: %d"%self.total_page_faults)
-        print("")
+        print("Total Page Fault(s): %d"%self.total_page_faults)
+        print("====================================================")
